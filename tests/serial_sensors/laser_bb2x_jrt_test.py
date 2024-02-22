2e349c4168607c0d9c778109b4022daad199512a
wuihee <wuihee@gmail.com>
2024-02-22 09:30:56 -0800

    Updated unit tests

--- tests/serial_sensors/laser_bb2x_jrt_test.py
+++
@@ -1,8 +0,0 @@
-from cycling_safety_sensors.serial_sensors import LaserBB2XJRT
-
-
-def test_get_distance(serial_mock):
-    serial_mock("AA 00 00 22 00 03 00 00 0B 5A 02 A4 30")
-    sensor = LaserBB2XJRT("/dev/ttyS0")
-    distance = sensor.get_distance()
-    assert distance == 58
